f01d4351a6f75dc458a30f0ad32051b05cc2e8a5
misa30122 <misa_30122@hotmail.com>
2022-04-01 20:12:19 -0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
+// TODO CHANGE THIS AS REACT 18 (NOT AVAILABLE YET)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
